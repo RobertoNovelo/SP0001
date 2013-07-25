765427b23f89311453bca21ce662f69d7dd50ae8
Roberto Rey <roberto.reym@gmail.com>
2013-07-25 00:29:00 -0500

    "Otros" tab funtionality hass been merged: "Datos del contacto" has been added. "Ayuda" has been added. "Acerca de la App" has been added. AndroidManifest phone permission was added.

---
+++ BombasMejoradaPreviewProject/BombasMejoradaPreview/src/main/java/com/smartplace/bombasmejorada/tabs/otros/HelpFragment.java
@@ -0,0 +1,74 @@
+package com.smartplace.bombasmejorada.tabs.otros;
+
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.smartplace.bombasmejorada.R;
+
+/**
+ * Created by Roberto on 12/07/13.
+ */
+
+
+/**
+ * Created by Roberto on 11/07/13.
+ */
+
+/*
+*   INTEGRATION HINTS:
+*
+*   DON´T FORGET TO ADD PERMISSION INTO ANDROID MANIFEST
+*   <uses-permission android:name="android.permission.CALL_PHONE"></uses-permission>
+*   <uses-permission android:name="android.permission.READ_PHONE_STATE" />
+*   layout: help_fragment.xml
+*   menu:
+*   values: help.xml
+*
+* */
+public class HelpFragment extends Fragment {
+
+    WebView helpWebView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        ActionBar abar = getActivity().getActionBar();
+        abar.setTitle("Ayuda");
+        abar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#195259")));
+        setHasOptionsMenu(true);
+        View view = inflater.inflate(R.layout.help_fragment, container, false);
+        helpWebView = (WebView) view.findViewById(R.id.help_webview);
+        helpWebView.setVerticalScrollBarEnabled(false);
+        helpWebView.loadData(getString(R.string.help_text), "text/html", "utf-8");
+        return view;
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.tab_otros, menu);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return  true;
+
+    }
+
+}
